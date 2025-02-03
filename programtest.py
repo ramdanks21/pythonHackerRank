1992141f3e6a2a82688f8d7dfc54a02afa753754
ramdanks21 <ramdankusumah2@gmail.com>
2025-02-03 15:13:18 +0700

    modief

--- programtest.py
+++ programtest.py
@@ -1,5 +1,19 @@
+def wrapper(f):
+    def fun(l):
+        formatted_numbers = []
+        for number in l:
+            #mengambil 10 digit terakhir dari nomor
+            clean_number = number[-10:]
+            #memformat ke bentuk "+91 xxxxx xxxxx"
+            formatted_number = f"+91 {clean_number[:5]} {clean_number[5:]}"
+            formatted_numbers.append(formatted_number)
+        return f(formatted_numbers)
+    return fun
+
+@wrapper
+def sort_phone(l):
+    print(*sorted(l), sep='\n')
+
 if __name__ == '__main__':
-    n = int(input())
-    
-    for x in range(1,n+1):
-        print(x,end='')
+    l = [input() for _ in range(int(input()))]
+    sort_phone(l) 
