b57c0a24497565eb6ff1409774a6f645add5a121
ramdanks21 <ramdankusumah2@gmail.com>
2025-02-04 08:45:35 +0700

    task completed

---
+++ swap-case.py
@@ -0,0 +1,10 @@
+def swap_case(s):
+    return s.swapcase()
+
+if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)
+
+    #swapcase() = digunakan untuk menukar tulisan kecil menjadi besar tulisan besar menjadi tulisan kecil contoh:
+    #aBcD = AbCd
