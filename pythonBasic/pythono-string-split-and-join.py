eabb35f4f44cd8259aee13b6f857520ddb907254
ramdanks21 <ramdankusumah2@gmail.com>
2025-02-05 08:33:18 +0700

    task completed

--- pythonBasic/pythono-string-split-and-join.py
+++ pythonBasic/pythono-string-split-and-join.py
@@ -1,9 +1,11 @@
 
-
+# Membuat sebuah string split and join
+#ouptu diharapkan this-is-a-string
 def split_and_join(line):
     # write your code here
+    return "-".join(line.split())
 
-if __name__ == '__main__   ':
-    line = input   ()
+if __name__ == '__main__':
+    line = input()
     result = split_and_join(line)
     print(result)
